8eed8ab149c4003e85c1a2e4024e6f480091f694
Emre <35729378+EmreOlgunIT@users.noreply.github.com>
2023-10-26 20:45:24 +0200

    Created Show repository

---
+++ src/main/java/com/example/tvshows/show/ShowRepository.java
@@ -0,0 +1,9 @@
+package com.example.tvshows.show;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ShowRepository extends JpaRepository<Show, Integer> {
+
+}
