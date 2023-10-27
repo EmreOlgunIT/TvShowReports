2f5e9d2157824e7c48afc6acd733dd4e42aaf70e
Emre <35729378+EmreOlgunIT@users.noreply.github.com>
2023-10-27 22:08:43 +0200

    API endpoint for activating createTop10RatedShowsReport method

---
+++ src/main/java/com/example/tvshows/report/ReportController.java
@@ -0,0 +1,24 @@
+package com.example.tvshows.report;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "api/report")
+public class ReportController {
+
+    private final ReportService reportService;
+
+    @Autowired
+    public ReportController(ReportService reportService) {
+        this.reportService = reportService;
+    }
+
+    @GetMapping("/top10")
+    public void top10(){
+        reportService.createTop10RatedShowsReport("top_10_shows_report");
+    }
+
+}
