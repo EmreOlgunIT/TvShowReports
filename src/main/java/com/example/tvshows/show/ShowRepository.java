243225d64c98db1739ca369d10c3c9ee271c77e9
Emre <35729378+EmreOlgunIT@users.noreply.github.com>
2023-10-27 22:03:40 +0200

    Created getTop10RatedShows method in ShowsRepository

--- src/main/java/com/example/tvshows/show/ShowRepository.java
+++ src/main/java/com/example/tvshows/show/ShowRepository.java
@@ -1,9 +1,16 @@
 package com.example.tvshows.show;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface ShowRepository extends JpaRepository<Show, Integer> {
+
+    @Query("SELECT s FROM Show s ORDER BY s.rating DESC LIMIT 10")
+    Optional<List<Show>> getTop10RatedShows();
 
 }
