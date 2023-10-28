6a7c532c7f0ddd7acca42d339138cb49f7104b0f
Emre <35729378+EmreOlgunIT@users.noreply.github.com>
2023-10-28 15:58:08 +0200

    Created methods for getting shows including genres, amount of episodes per show, and amount of released episodes per show

--- src/main/java/com/example/tvshows/show/ShowRepository.java
+++ src/main/java/com/example/tvshows/show/ShowRepository.java
@@ -2,6 +2,7 @@ package com.example.tvshows.show;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -11,6 +12,14 @@ import java.util.Optional;
 public interface ShowRepository extends JpaRepository<Show, Integer> {
 
     @Query("SELECT s FROM Show s ORDER BY s.rating DESC LIMIT 10")
-    Optional<List<Show>> getTop10RatedShows();
+    List<Show> getTop10RatedShows();
 
+    @Query("SELECT s FROM Show s LEFT JOIN FETCH s.genres")
+    List<Show> getAllShowsIncludingGenres();
+
+    @Query("SELECT s.id, COUNT(e) FROM Show s JOIN s.episodes e GROUP BY s.id")
+    List<Object[]> getAmountOfEpisodesPerShow();
+
+    @Query("SELECT s.id, COUNT(e) FROM Show s JOIN s.episodes e WHERE e.releaseUnixTime < :unixTimestamp GROUP BY s.id")
+    List<Object[]> getAmountOfReleasedEpisodesPerShowBeforeUnixTimestamp(@Param("unixTimestamp") long unixTimestamp);
 }
---
+++ src/main/java/com/example/tvshows/show/ShowService.java
@@ -0,0 +1,57 @@
+package com.example.tvshows.show;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.util.HashMap;
+import java.util.List;
+
+@Service
+public class ShowService {
+
+    private final ShowRepository showRepository;
+
+    @Autowired
+    public ShowService(ShowRepository showRepository) {
+        this.showRepository = showRepository;
+    }
+
+    public List<Show> getAllShows() {
+        return showRepository.findAll();
+    }
+
+    public List<Show> getTop10RatedShows() {
+        return showRepository.getTop10RatedShows();
+    }
+
+    public List<Show> getAllShowsIncludingGenres() {
+        return showRepository.getAllShowsIncludingGenres();
+    }
+
+    public HashMap<Object, Object> getAmountOfEpisodesPerShowMap() {
+        HashMap<Object, Object> amountOfEpisodesMap = new HashMap<>();
+        List<Object[]> amountOfEpisodesPerShow = showRepository.getAmountOfEpisodesPerShow();
+
+        for (Object[] objArray : amountOfEpisodesPerShow) {
+            amountOfEpisodesMap.put(objArray[0], objArray[1]);
+        }
+
+        return amountOfEpisodesMap;
+    }
+
+    public HashMap<Object, Object> getAmountOfReleasedEpisodesPerShowMap() {
+        Instant currentTimestamp = Instant.now();
+        long currentUnixTimestamp = currentTimestamp.getEpochSecond();
+        List<Object[]> amountOfReleasedEpisodesPerShow = showRepository.getAmountOfReleasedEpisodesPerShowBeforeUnixTimestamp(currentUnixTimestamp);
+
+        HashMap<Object, Object> amountOfReleasedEpisodesPerShowMap = new HashMap<>();
+
+        for (Object[] objArray : amountOfReleasedEpisodesPerShow) {
+            amountOfReleasedEpisodesPerShowMap.put(objArray[0], objArray[1]);
+        }
+
+        return amountOfReleasedEpisodesPerShowMap;
+    }
+
+}
