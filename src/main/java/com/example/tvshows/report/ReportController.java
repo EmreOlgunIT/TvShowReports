6d5a386c989ecf7ef000184c2f6d004e46576034
Emre <35729378+EmreOlgunIT@users.noreply.github.com>
2023-10-29 23:03:16 +0100

    ReportController API endpoints now serve requested report .txt files

--- src/main/java/com/example/tvshows/report/ReportController.java
+++ src/main/java/com/example/tvshows/report/ReportController.java
@@ -1,10 +1,14 @@
 package com.example.tvshows.report;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
 
 @RestController
 @RequestMapping(path = "api/report")
@@ -18,28 +22,44 @@ public class ReportController {
     }
 
     @GetMapping("/top10")
-    public void top10(){
-        reportService.createTop10RatedShowsReport("top_10_shows_report");
+    public ResponseEntity<byte[]> top10() {
+        return ResponseEntity.ok()
+                .header("Content-Disposition", "attachment; filename=top_10_shows_report.txt")
+                .contentType(MediaType.TEXT_PLAIN)
+                .body(reportService.createTop10RatedShowsReport());
     }
 
     @GetMapping("/summary")
-    public void summary(){
-        reportService.createSummaryReport("summary_report");
+    public ResponseEntity<byte[]> summary() {
+        return ResponseEntity.ok()
+                .header("Content-Disposition", "attachment; filename=summary_report.txt")
+                .contentType(MediaType.TEXT_PLAIN)
+                .body(reportService.createSummaryReport());
     }
 
     @GetMapping("/best-episode-per-show")
-    public void bestEpisodePerShow(){
-        reportService.createBestEpisodePerShowReport("best_episode_per_show_report");
+    public ResponseEntity<byte[]> bestEpisodePerShow() {
+
+        return ResponseEntity.ok()
+                .header("Content-Disposition", "attachment; filename=best_episode_per_show_report.txt")
+                .contentType(MediaType.TEXT_PLAIN)
+                .body(reportService.createBestEpisodePerShowReport());
     }
 
     @GetMapping("/recommend")
-    public void recommend(@RequestParam(required = true) String genre){
-        reportService.createRecommendedShowReport("recommended_show_report", genre);
+    public ResponseEntity<byte[]> recommend(@RequestParam(required = true) String genre) throws IOException {
+        return ResponseEntity.ok()
+                .header("Content-Disposition", "attachment; filename=recommend_report.txt")
+                .contentType(MediaType.TEXT_PLAIN)
+                .body(reportService.createRecommendedShowReport(genre));
     }
 
     @GetMapping("/nextweek")
-    public void nextWeek(){
-        reportService.createNextWeekReport("nextweek_report");
+    public ResponseEntity<byte[]> getNextWeekReport() throws IOException {
+        return ResponseEntity.ok()
+                .header("Content-Disposition", "attachment; filename=nextweek_report.txt")
+                .contentType(MediaType.TEXT_PLAIN)
+                .body(reportService.createNextWeekReport());
     }
 
 }
